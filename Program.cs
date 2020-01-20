e573d372bf98d4a3f8a882f209ba60c2feceb9df
Patrick Williams <51858277+GeniusAlg0s@users.noreply.github.com>
2020-01-20 12:37:52 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int number;
+            number = 3;
+
+            const float pi = 3.14f;
+            float totalPrice = 20.95f;
+            string me = "pat";
+            byte lumber = 2;
+            Console.WriteLine(lumber + " :byte" + number + " :number" + pi + " :float");
+            Console.WriteLine(me + " :string"  + " :number" + pi + " :float");
+           
+        }
+
+    }
+}
